f1c2d2d4bff0da6e2a687d8683634cfca5e2e689
suraj <surajgit12@gmail.com>
2024-01-23 17:23:12 +0530

    My 2nd commit

---
+++ src/test/java/Session01/TestCase02.java
@@ -0,0 +1,13 @@
+package Session01;
+import org.testng.annotations.Test;
+
+public class TestCase02 {
+	
+	@Test
+	public void createUser() {
+		
+		System.out.println("Git 2nd command");
+		
+	}
+
+}
