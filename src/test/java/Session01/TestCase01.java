b6ad37c70bdb456be2a9a6e11d3b0021160a994d
suraj <surajgit12@gmail.com>
2024-01-23 17:09:02 +0530

    My first commit

---
+++ src/test/java/Session01/TestCase01.java
@@ -0,0 +1,14 @@
+package Session01;
+
+import org.testng.annotations.Test;
+
+public class TestCase01 {
+	
+	@Test
+	public void createUser() {
+		
+		System.out.println("Git 1st command");
+		
+	}
+
+}
