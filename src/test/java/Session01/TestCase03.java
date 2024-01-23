6292bf1fbd7735b47df6258cd38e69d3bdefdcaf
suraj <surajgit12@gmail.com>
2024-01-23 17:33:10 +0530

    new class has been added

---
+++ src/test/java/Session01/TestCase03.java
@@ -0,0 +1,17 @@
+package Session01;
+
+import org.testng.annotations.Test;
+
+public class TestCase03 {
+
+	
+	@Test
+	public void createUser() {
+		
+		System.out.println("Git 3rd command");
+		
+		
+	}
+
+}
+}
